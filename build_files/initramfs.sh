5ad200f3bc3907d8c29b1e3c7e778ac891960de1
Universelord <113364513+Universelord@users.noreply.github.com>
2026-06-21 18:44:45 +0530

    uhhh another test but it will not work

--- build_files/initramfs.sh
+++ build_files/initramfs.sh
@@ -5,8 +5,6 @@ add_dracutmodules+=" plymouth "
 hostonly=no
 EOF
 
-# Regenerate initramfs with Plymouth
-KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
-export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
-chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+plymouth-set-default-theme bgrt
+
+dracut --force --kver $(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')
