8ccf6f4b7d0fcf617f02e83cacff0f8b0f5269b7
Universelord <113364513+Universelord@users.noreply.github.com>
2026-06-25 21:48:22 +0530

    So I found that a install through iso was able to do work with plymouth for some reason, idk why, so i made my the install things in another file

--- Containerfile
+++ Containerfile
@@ -36,6 +36,7 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh
+    # /ctx/install.sh
 
 ### LINTING
 ## Verify final image and contents are correct.
--- build_files/build.sh
+++ build_files/build.sh
@@ -27,28 +27,4 @@ cp -avf "/ctx/system_files"/. /
 systemctl enable podman.socket
 
 # INFO: Universe_lord's modifications
-
-dnf5 install -y \
-    python3 python3-pip python3-devel python3-idle uv \
-    neovim git bat ripgrep zoxide tmux fish \
-    gwenview kclock filelight kate kcalc kontact okular skanpage\
-    distrobox chromium
-
-
-tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
-[gitlab.com_paulcarroty_vscodium_repo]
-name=gitlab.com_paulcarroty_vscodium_repo
-baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
-metadata_expire=1h
-EOF
-
-dnf5 install -y codium
-
-flatpak preinstall -y
-
-# Regenerate initramfs with Plymouth support
-dracut -f --add-drivers "i915" --add-drivers "nouveau" --add-drivers "amdgpu"
+/ctx/install.sh
